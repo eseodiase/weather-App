f32c800f4a45d1225d4b9251f4cd7c706ddbaa40
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-08-07 10:13:58 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import {Homepage, Aboutpage, Header } from './App'; //new
+import { Link } from 'react-router-dom';
 
-ReactDOM.render(
-    <Router>
-       <Switch>
-					<Header/> //new
-		      <Route exact path="/" component={Homepage}/>
-				<Route exact path="/about" component={Aboutpage }/>
-	    </Switch>
-    </Router>,
-    document.getElementById('root')
-);
+const Header = () => {
+	return (
+		<div>
+				<p>Header</p>
+		</div>
+	)
+};
+
+const Homepage = () => {
+	return (
+		<div>
+				<h1>Homepage </h1>
+				<Link to='/about'>Go to Aboutpage</Link>
+		</div>
+	)
+};
+
+const Aboutpage = () => {
+	return (
+		<div>
+				<h1>Aboutpage</h1>
+				<Link to='/'>Go to Aboutpage</Link>
+		</div>
+	)
+};
+export {Homepage, Aboutpage , Header } ; //new
